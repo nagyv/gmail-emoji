93a3825354203864a784dbe9f08ef6ed630de16d
Viktor Nagy <viktor.nagy@gmail.com>
2015-08-19 09:01:25 +0200

    nicer popup, SHIFT handling

--- emojis.js
+++ emojis.js
@@ -1,9 +1,30 @@
 var forEach = Array.prototype.forEach;
 var $status;
+var counter = 0;
 
 function handleClick(event) {
-    var range = document.createRange();  
-    range.selectNode(event.target);  
+    var more = false;
+    if(event.shiftKey) {
+        more = true;
+    }
+
+    if(counter > 0) {
+        $status.innerHTML += '<img src="' + event.target.src + '" />'
+    } else {
+        $status.innerHTML = '<img src="' + event.target.src + '" />'
+    }
+    counter++;
+
+    if(!more) {
+        copyStatus();
+    } else {
+        showButtons();
+    }
+}
+
+function copyStatus() {
+    var range = document.createRange();
+    range.selectNodeContents($status);  
     window.getSelection().addRange(range);  
 
     try {  
@@ -18,23 +39,30 @@ function handleClick(event) {
         window.getSelection().removeAllRanges();
     }
 
-    if (successful) {
-        showNote();
-    }
-}
-
-function showNote() {
-    $status.innerHTML = 'Copied';
     window.setTimeout(function(){
         window.close();
-    }, 1000);
+    }, 500);
 }
 
+function showButtons() {
+    document.querySelector('div.buttons').classList.add('visible');
+}
+function hideButtons() {
+    document.querySelector('div.buttons').classList.remove('visible');
+}
+
+function clear() {
+    $status.innerHTML = 'Clik an icon to copy it.';
+    counter = 0;
+    hideButtons();
+}
 
 document.addEventListener('DOMContentLoaded', function(){
     $status = document.getElementById('status');
     var images = document.getElementsByTagName('img');
     forEach.call(images, function( image ){
       image.addEventListener('click', handleClick);
-    })
-})
+    });
+    document.getElementById('clear').addEventListener('click', clear);
+    document.getElementById('copy').addEventListener('click', copyStatus);
+});
